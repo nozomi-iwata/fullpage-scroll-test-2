781c7fb0c9c45a87e02c2fec66c8eb4418e3cebf
nozomi-iwata <145537630+nozomi-iwata@users.noreply.github.com>
2024-10-31 11:55:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['home', 'content', 'footer'],
+     //options here
+     autoScrolling:true,
+     scrollHorizontally: true,
+     navigation: true, 
+
+    // Get your license at https://alvarotrigo.com/fullPage/pricing
+    licenseKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+});
